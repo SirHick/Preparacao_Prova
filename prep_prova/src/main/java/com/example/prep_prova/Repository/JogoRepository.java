e4cd9fe1d5083410d9bbb6cb41f630b7a4252cfd
Henrique Miguel Deschamps Dahmer <henriquedahmer1234@gmail.com>
2026-06-03 08:48:28 -0300

    Delete prep_prova directory

--- prep_prova/src/main/java/com/example/prep_prova/Repository/JogoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.prep_prova.Repository;
-
-import com.example.prep_prova.Model.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogoRepository extends JpaRepository <Jogo, Long> {
-}
